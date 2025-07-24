bb8e7ed1b5df5fe09fe7a7c724ccf52a2c513d08
gundlarakesh <rakeshgundla68@gmail.com>
2025-07-24 22:48:49 +0530

    updated cli args file

--- cli_args.sh
+++ cli_args.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+#########################
+# Author: Rakesh Gundla
+#
+# Date: 24-07-2025
+#
+# version: v1
+#########################
+
+
+
 # set -x # uncomment to run in debug mode
 set -e # comment this line to ignore the errors
 
 echo "your first argument is $0 'name of the script file'"
 echo "2nd args is $1"
+echo "Total no of args passed $#"
+echo "all args as single sting '$@'"
+echo "$*"
