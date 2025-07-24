5f37117d363b60c91ade53712f9b301a4b193358
gundlarakesh <rakeshgundla68@gmail.com>
2025-07-24 23:24:48 +0530

    practice file

---
+++ first.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x # "for debug mode"
+
+set -e # stop the execution when error occurred
+
+set -o # catch the error in pipeline
+
+echo "my name i"
+
+# curl send request and print the out
+# wget download the file
