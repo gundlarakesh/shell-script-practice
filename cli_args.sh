b4a063e31f1d21b04bb7a0aaf1c88b7e116c138d
gundlarakesh <rakeshgundla68@gmail.com>
2025-07-24 22:42:47 +0530

    how to take the agrs from cli

---
+++ cli_args.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# set -x # uncomment to run in debug mode
+set -e # comment this line to ignore the errors
+
+echo "your first argument is $0 'name of the script file'"
+echo "2nd args is $1"
